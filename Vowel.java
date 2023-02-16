995778b412872c9e4f7c8df95ec77d768db458b4
akash <akashyewale007@gmail.com>
2023-02-16 21:58:00 +0530

    day 2 switch

---
+++ Vowel.java
@@ -0,0 +1,36 @@
+package DAY2ASSINGMENT;
+
+public class Vowel {
+
+	public static void main(String[] args) {
+		
+	 char ch='E';
+	 
+	 switch(ch) {
+	 
+	 case 'A': 
+	 
+	 case 'E': 
+	 
+	 case 'I': 
+	 
+	 case 'O': 
+	 
+	 case 'U': 
+	 
+	 case 'Y': 
+	 
+	 case 'W': 
+		 System.out.println("vowel");
+		 break;
+	 
+	 default: System.out.println("Consonant");
+		 
+	 }
+	 
+	 
+		 
+
+	}
+
+}
